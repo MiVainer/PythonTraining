8b4b6ccb577dc1c62d82f25272c547bf9869b31c
Mihail Vainer <mh93@yandex.ru>
2023-04-12 22:04:42 +0700

    12.04.23

--- 4_Dict.py
+++ 4_Dict.py
@@ -16,7 +16,7 @@ del(PhoneNumbers["Михаил"]) # Удаляем элемент словаря
 print(PhoneNumbers)
 #PhoneNumbers.clear() # Удаляем все элементы словаря
 #print(PhoneNumbers)
-print("Папа" in PhoneNumbers) #Проверка на вхождение '''
+print("Папа" in PhoneNumbers) #Проверка на вхождение
 
 enemy = {
             'loc_x': 70,
@@ -52,3 +52,91 @@ print('-----------------------------------------------------------------')
 
 for k in all_enemies:
     print('Враги изменены', k)
+
+# Задачи
+# Объединить словари в один
+dic1 = {1:10, 2:20}
+dic2 = {3:30, 4:40}
+dic3 = {5:50,6:60}
+dic4 = dict(list(dic1.items())+list(dic2.items())+list(dic3.items()))
+print(dic4)
+
+# Проверить есть ли ключ в словаре
+enemy = {
+            'loc_x': 70,
+            'loc_y': 50,
+            'color': 'red',
+            'health': 100,
+            'name': 'Ivan',
+            'weight': 70,
+            'might': 'low'
+}
+def key_in_dict(x):
+     if x in enemy:
+         print('This key is exist')
+     else:
+         print('Key is not exist')
+     return
+
+key_in_dict('nam')
+
+# Написать программу для перебора словаря
+
+enemy = {
+            'loc_x': 70,
+            'loc_y': 50,
+            'color': 'red',
+            'health': 100,
+            'name': 'Ivan',
+            'weight': 70,
+            'might': 'low'
+}
+for dict_key, dict_volue in enemy.items():
+    print(dict_key,'->', dict_volue)
+
+# Напишите скрипт Python для генерации и печати словаря, который содержит число (от 1 до n) в форме (x, x * x).
+
+dict = {a: a*a for a in range(5)}
+print(f'Вот такой словарь у тебя получился {dict}')
+
+# Напишите скрипт Python для печати словаря, в котором ключи - это числа от 1 до 15 (оба включены), а значения - квадрат ключей.
+
+dict = {a: a*a for a in range(1, 16)}
+print(f'Вот такой словарь у тебя получился {dict}')
+
+# Напишите скрипт Python для объединения двух словарей Python.
+a = {'a': 10, 'b': 20}
+b = {'c': 30}
+a.update(b)
+print(a)
+
+# Напишите программу на Python для суммирования всех элементов в словаре.
+# 1 метод
+a = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
+sum = 0
+for i in a.values():
+    sum = sum+i
+print('1 метод - ', sum)
+# 2 метод
+a = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81, 10: 100, 11: 121, 12: 144, 13: 169, 14: 196, 15: 225}
+print('Второй метод - ', sum(a.values()))
+
+# Напишите программу на Python для отображения двух списков в словарь.
+keys = ['red', 'green', 'blue']
+values = ['#FF0000','#008000', '#0000FF']
+color_dictionary = dict(zip(keys, values))
+print(color_dictionary)
+
+# Напишите программу на Python для сортировки словаря по ключу.
+a = {'n': 50, 'a': 10, 'b': 20, 'k': 100}
+sorted_tuple = sorted(a.items(), key=lambda x: x[0])
+print(dict(sorted_tuple))
+
+#Напишите программу на Python, чтобы получить максимальное и минимальное значение в словаре.
+a = {'n': 50, 'a': 10, 'b': 20, 'k': 100}
+ma = max(a.values())
+mi = min(a.values())
+print(f'Максимальное значение - {ma}\nМинимальное значение - {mi}')'''
+
+
+
