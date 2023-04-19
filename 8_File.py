08ba019c508b0c2bade35ebc2597357c5bdf8d0b
Mihail Vainer <mh93@yandex.ru>
2023-04-19 21:46:13 +0700

    19.04.2023

--- 8_File.py
+++ 8_File.py
@@ -15,3 +15,15 @@ myfile1.close()'''
 
 # Пишем скрипт для выборки из логов подключенных флэш-накопителей к ПЭВМ с датой, временем, серийником
 
+inputfile = f"/var/log/{input('Введи имя файла для парсинга в папке /var/log- ')}"
+outputfile = f"/home/mihail/parsing/{input('Введи имя файла для сохранения результата в каталоге /home/mihail/parsing- ')}"
+
+search_str = "SerialNumber: "
+myfile = open(inputfile, mode='r', encoding='ascii')
+myfile1 = open(outputfile, mode='w', encoding='ascii')
+for line in myfile:
+    if search_str in line:
+        myfile1.write(line + '\n')
+
+myfile.close()
+myfile1.close()
